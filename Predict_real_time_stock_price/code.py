d12681da87a490339e9d2075e61311373bd08714
ayresdiego <diego.ayres.br@gmail.com>
2021-01-21 13:07:44 +0100

    readme

--- Predict_real_time_stock_price/code.py
+++ Predict_real_time_stock_price/code.py
@@ -1,7 +1,6 @@
-
+import matplotlib.pyplot as plt
 import pandas as pd
 import yfinance as yf
-import matplotlib.pyplot as plt
 
 
 def yfinance_data(company_id):
@@ -11,15 +10,14 @@ def yfinance_data(company_id):
     series_data_historical = tickers.history(period="max")
     df_1 = pd.DataFrame(series_data_historical)
 
-    df_1['company_id'] = company_id
-    df_1['series_type'] = 'historical_data'
+    df_1["company_id"] = company_id
+    df_1["series_type"] = "historical_data"
 
     df_1 = df_1.sort_index(ascending=True)
 
-    historical_price = df_1['Close']
+    historical_price = df_1["Close"]
 
     return historical_price
-
 
 
 def prophet_model(df, holidays=True):
@@ -28,7 +26,7 @@ def prophet_model(df, holidays=True):
 
     model_x = Prophet()
     if holidays == True:
-        model_x.add_country_holidays(country_name='US')
+        model_x.add_country_holidays(country_name="US")
 
     model_x.fit(df)
 
@@ -37,14 +35,13 @@ def prophet_model(df, holidays=True):
     print("predictions DF:")
     print(prediction.head().to_string())
 
-    model_x.plot(prediction) # data point as scatter; rolling avg; confidence interval
+    model_x.plot(prediction)  # data point as scatter; rolling avg; confidence interval
     plt.title(f"{company_id}: Future price prediction")
     plt.tight_layout()
     plt.show()
 
     model_x.plot_components(prediction)
     plt.show()
-
 
 
 company_id = "GOOGL"
