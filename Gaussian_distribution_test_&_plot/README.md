d135030136987aa202a75038b49278f41e159925
Diego Ayres <62593970+ayresdiego@users.noreply.github.com>
2021-02-05 22:52:17 +0100

    Add files via upload

---
+++ Gaussian_distribution_test_&_plot/README.md
@@ -0,0 +1,14 @@
+## Gaussian distribution 
+
+> This python function to evidence if sample follows a Gaussian distribution (Normal distribution)
+
+- Shapiro-Wilk test
+- Anderson-Darling test
+- Quantile-Quantile Plot (Q-Q plot)
+- Kernel Density Estimation (KDE)
+- Gaussian distribution 
+- Histogram
+
+**Required libaries**: *scipy, statsmodels, numpy, seaborn, math ,pandas, matplotlib*
+
+![Image Detection](plot1.png)
---
+++ Gaussian_distribution_test_&_plot/code.py
@@ -0,0 +1,96 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+import seaborn as sns
+import math
+import scipy.stats as stats
+import statsmodels.api as sm
+
+
+def normality_test(test_data):
+
+    test_data = np.array(test_data)
+    test_data = np.ravel(test_data)
+
+    import scipy.stats as stats
+    asymmetry = stats.skew(test_data)
+    print('Skewness: {0:.4f}'.format(asymmetry))
+
+    kurtosis = stats.kurtosis(test_data)
+    print('Kurtosis: {0:.4f}'.format(kurtosis))
+
+    # D’Agostino, R. and Pearson
+    stat, p1 = stats.normaltest(test_data)
+    print("\nD’Agostino, R. and Pearson Statistics= %.3f, p=%.3f" % (stat, p1))
+    alpha = 0.05
+    if p1 < alpha: # (reject null hypothesis)
+        print('\033[1;30;41mNot Normal distribution\33[m')
+    else:
+        print('\033[1;30;46mNormal distribution\33[m')
+
+
+    # Shapiro - Wilk test
+    W, p2 = stats.shapiro(test_data)  # test for normality
+    print('\nShapiro-Wilk: W:{0:.4f}    P={1:.4f}'.format(W, p2))
+    alpha = 0.05
+    if p2 < alpha:  # (reject null hypothesis)
+        print('\033[1;30;41mNot Normal distribution\33[m')  # Data looks non-Gaussian
+    else:
+        print('\033[1;30;46mNormal distribution\33[m')  # Data looks Gaussian (fail to reject null hypothesis)
+
+    sns.set() # give a better layout
+
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(13, 6), dpi=80, sharex=False)
+    fig.subplots_adjust(hspace=0.2, wspace=0.2)
+
+    sns.histplot(test_data
+                 , bins=40
+                 , stat = "density"
+                 , color='c'
+                 , alpha=0.6
+                 , label='histogram'
+                 , ax=ax1
+                 )
+
+
+    sns.kdeplot(test_data
+                , color='darkblue'
+                 , label='KDE - kernel density estimation'
+                 , ax=ax1
+                 )
+
+    import scipy.stats as stats
+    avg = test_data.mean()
+    sigma = math.sqrt(test_data.var())
+    x = np.linspace(avg - 3 * sigma, avg + 3 * sigma, 100)
+    ax1.plot(x, stats.norm.pdf(x, avg, sigma)
+             , color='yellow', alpha=0.8, linewidth=2, label='normal distribution')
+    ax1.legend(loc='best', fontsize='x-small', title=None, bbox_to_anchor=None).set_draggable(True)
+
+
+    # Theoretical Quantiles
+    import statsmodels.api as sm
+    sm.qqplot(test_data
+              , line='s'
+              , fit=True
+              , ax=ax2)
+
+    ax2.set_title("(QQ) Quantile-Quantile Plot")
+
+    plt.tight_layout()h
+    plt.show()
+    fig.savefig('plot.png')
+
+    return
+
+mu, sigma = 30, 1
+normal_dist = s = np.random.normal(mu, sigma, 1000)
+normality_test(normal_dist)
+
+
+df = pd.read_csv("test_data.csv"
+                 , delimiter=";")
+normality_test(df)
+
